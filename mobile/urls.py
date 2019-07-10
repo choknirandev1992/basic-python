7672a155b0035d088e671691d84408d8a127fe4f
choknirandev1992 <chokniran@gmail.com>
2019-07-10 16:16:36 +0700

    add model ems and product

--- mobile/urls.py
+++ mobile/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Home,About,Product
+from .views import Home,About,Product,EMStracking
 
 urlpatterns = [
     path('',Home, name='mobile-home' ),
     path('about/',About, name='mobile-about' ),
+    path('ems/',EMStracking, name='mobile-ems' ),
     path('product/',Product, name='mobile-product' ),
 ]
+
--- mobile/views.py
+++ mobile/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, EMS
 
 # Create your views here.
 
 def Home(request):
     #return HttpResponse('<h1>Hello world</h1>')
-    return render(request,'mobile/home.html')
+    allmobile = Product.objects.all()
+    #context = {'phonelist': ['I Phone X','Heuwei','samsung','oppo']}
+    context = { 'phonelist': allmobile ,'title': 'Home'}
+    return render(request, 'mobile/home.html', context)
+
+def EMStracking(request):
+    #return HttpResponse('<h1>Hello world</h1>')
+    allems = EMS.objects.all()
+    #context = {'phonelist': ['I Phone X','Heuwei','samsung','oppo']}
+    context = { 'emslist': allems ,'title': 'EMS'}
+    return render(request, 'mobile/ems.html', context)
 
 def About(request):
     #return HttpResponse('<h1>About US</h1>')
-    return render(request,'mobile/about.html')
+    return render(request,'mobile/about.html')  
 
-def Product(request):
-    #return HttpResponse('<h1>Product</h1>')
-    return render(request,'mobile/product.html')
 
+   
